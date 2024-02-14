3f07f01645a326e38b1ca0c65908fc5429da62ac
jx_kurbanov <kurbanov.j.kh@gmail.com>
2024-02-14 17:27:10 +0500

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import { resolve } from 'pathe'
 import { defineConfig } from 'vitest/config'
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  resolve: { alias: { src: resolve('src/') } },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'RsImzo',
+      formats: ['iife', 'cjs', 'es', 'umd']
+    },
+    sourcemap: true,
+  },
   test: {
     environment: 'jsdom',
   },
+  plugins: [dts()]
 })
